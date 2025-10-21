46a29c7bad519549027eb33e33d47b7a388af8ee
mahesh <mahesh2885858@gmail.com>
2025-10-21 20:32:10 +0530

    feat: add some fields

--- src/screens/TransactionDetails/index.tsx
+++ src/screens/TransactionDetails/index.tsx
@@ -1,26 +1,30 @@
-import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
-import React from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { formatDigits } from 'commonutil-core';
+import { format } from 'date-fns';
+import React, { useMemo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Avatar, Icon } from 'react-native-paper';
-import { spacing, textSize, useAppTheme } from '../../../theme';
+import { borderRadius, spacing, textSize, useAppTheme } from '../../../theme';
 import { gs } from '../../common';
-import PressableWithFeedback from '../../components/atoms/PressableWithFeedback';
 import ScreenWithoutHeader from '../../components/molecules/ScreenWithoutHeader';
+import useTransactionsStore from '../../stores/transactionsStore';
 import { TRootStackParamList } from '../../types';
-const ICON_SIZE = 30;
-const AVATAR_SIZE = 40;
 
 const TransactionDetails = () => {
   const route =
     useRoute<RouteProp<TRootStackParamList, 'TransactionDetails'>>();
   const transaction = route.params.transaction;
-  const navigation = useNavigation();
   const { colors } = useAppTheme();
+  const categories = useTransactionsStore(state => state.categories);
+
+  const categoryName = useMemo(() => {
+    const category = categories.find(c => c.id === transaction.categoryIds[0]);
+    return category?.name ?? 'General';
+  }, [categories, transaction]);
 
   return (
     <ScreenWithoutHeader>
       {/* Header */}
-      <View style={[gs.flexRow, gs.itemsCenter, styles.header]}>
+      {/* <View style={[gs.flexRow, gs.itemsCenter, styles.header]}>
         <View
           style={[gs.flexRow, gs.itemsCenter, gs.fullFlex, styles.headerLeft]}
         >
@@ -42,6 +46,109 @@ const TransactionDetails = () => {
           size={AVATAR_SIZE}
           style={{ backgroundColor: colors.primaryContainer }}
         />
+      </View> */}
+      <View style={styles.screenContainer}>
+        <View>
+          <Text
+            style={[
+              styles.title,
+              {
+                color: colors.onSurfaceDisabled,
+              },
+            ]}
+          >
+            Amount
+          </Text>
+          <Text
+            style={[
+              styles.amountText,
+              {
+                color: colors.success,
+              },
+            ]}
+          >
+            {transaction.type === 'expense'
+              ? `-₹${formatDigits(transaction.amount.toString())}`
+              : `₹${formatDigits(transaction.amount.toString())}`}
+          </Text>
+        </View>
+        <View
+          style={[
+            {
+              gap: spacing.sm,
+            },
+          ]}
+        >
+          <Text
+            style={[
+              styles.title,
+              {
+                color: colors.onSurfaceDisabled,
+              },
+            ]}
+          >
+            Type
+          </Text>
+          <View style={[gs.flexRow]}>
+            <Text
+              style={[
+                styles.amountText,
+                {
+                  color: colors.onPrimaryContainer,
+                  backgroundColor: colors.primaryContainer,
+                  borderRadius: borderRadius.xxl,
+                  paddingHorizontal: spacing.md,
+                },
+              ]}
+            >
+              {transaction.type === 'expense' ? 'Expense' : 'Income'}
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Text
+            style={[
+              styles.title,
+              {
+                color: colors.onSurfaceDisabled,
+              },
+            ]}
+          >
+            Date
+          </Text>
+          <Text
+            style={[
+              styles.amountText,
+              {
+                color: colors.onPrimaryContainer,
+              },
+            ]}
+          >
+            {format(transaction.transactionDate, 'MMM d, yyyy')}
+          </Text>
+        </View>
+        <View>
+          <Text
+            style={[
+              styles.title,
+              {
+                color: colors.onSurfaceDisabled,
+              },
+            ]}
+          >
+            Category
+          </Text>
+          <Text
+            style={[
+              styles.amountText,
+              {
+                color: colors.onPrimaryContainer,
+              },
+            ]}
+          >
+            {categoryName}
+          </Text>
+        </View>
       </View>
     </ScreenWithoutHeader>
   );
@@ -59,5 +166,17 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     fontSize: textSize.lg,
+  },
+  screenContainer: {
+    paddingHorizontal: spacing.md,
+    gap: spacing.sm,
+  },
+  title: {
+    fontSize: textSize.lg,
+    fontWeight: 'semibold',
+  },
+  amountText: {
+    fontSize: textSize.xl,
+    fontWeight: 'bold',
   },
 });
