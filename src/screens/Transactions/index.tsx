22b0ecb22da0eed9301d64f4c357837aad433ce4
mahesh <mahesh2885858@gmail.com>
2026-01-03 23:26:50 +0530

    Add;gifted charts and init chart

--- src/screens/Transactions/index.tsx
+++ src/screens/Transactions/index.tsx
@@ -1,16 +1,20 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback, useEffect } from 'react';
-import { BackHandler, ScrollView, StyleSheet, View } from 'react-native';
+import { BackHandler, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { borderRadius, spacing, useAppTheme } from '../../../theme';
+import { borderRadius, spacing, textSize, useAppTheme } from '../../../theme';
 import CommonHeader from '../../components/organisms/CommonHeader';
 import useTransactionsStore from '../../stores/transactionsStore';
+import useGetTransactions from '../../hooks/useGetTransactions';
+import { gs } from '../../common';
+import { LineChart } from 'react-native-gifted-charts';
 
 const Transactions = () => {
   const { top } = useSafeAreaInsets();
-  const theme = useAppTheme();
+  const { colors } = useAppTheme();
   const navigation = useNavigation();
   const resetFilters = useTransactionsStore(state => state.resetFilters);
+  const { filteredTransactions } = useGetTransactions();
 
   const handleBackPress = useCallback(
     (isEvent = false) => {
@@ -49,11 +53,45 @@ const Transactions = () => {
       {/* recent transactions section */}
       <View
         style={[
+          gs.fullFlex,
           {
             paddingHorizontal: spacing.lg,
           },
         ]}
-      ></View>
+      >
+        {filteredTransactions.length < 2 ? (
+          <View style={[gs.fullFlex, gs.centerItems]}>
+            <Text
+              style={[
+                gs.fontBold,
+                {
+                  color: colors.onSurfaceVariant,
+                  fontSize: textSize.lg,
+                },
+              ]}
+            >
+              Not enough transactions!!
+            </Text>
+          </View>
+        ) : (
+          <View>
+            <LineChart
+              data={filteredTransactions
+                .filter(d => d.type === 'expense')
+                .map(t => ({ value: t.amount }))}
+              data2={filteredTransactions
+                .filter(d => d.type === 'income')
+                .map(t => ({ value: t.amount }))}
+              areaChart
+              color="skyblue"
+              color2="orange"
+              startFillColor="skyblue"
+              startFillColor2="orange"
+              curved
+            />
+          </View>
+        )}
+      </View>
     </ScrollView>
   );
 };
