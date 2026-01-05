9ee097bd484c6019fb70a9b44d0ce8b11cda4fcf
mahesh <mahesh2885858@gmail.com>
2026-01-05 22:21:30 +0530

    wip: check point one

--- src/screens/Transactions/index.tsx
+++ src/screens/Transactions/index.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/no-unstable-nested-components */
 import { useNavigation } from '@react-navigation/native';
 import { format } from 'date-fns';
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import {
   BackHandler,
   Dimensions,
@@ -18,18 +18,30 @@ import {
   Pointer,
 } from 'react-native-gifted-charts';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { spacing, textSize, useAppTheme } from '../../../theme';
+import { borderRadius, spacing, textSize, useAppTheme } from '../../../theme';
 import { gs } from '../../common';
 import CommonHeader from '../../components/organisms/CommonHeader';
 import useGetTransactions from '../../hooks/useGetTransactions';
 import useTransactionsStore from '../../stores/transactionsStore';
+import PressableWithFeedback from '../../components/atoms/PressableWithFeedback';
+import { TTransactionType } from '../../types';
+import { VictoryChart, VictoryArea } from 'victory-native';
 const { width } = Dimensions.get('window');
+console.log({ width });
 const Transactions = () => {
   const { top } = useSafeAreaInsets();
   const { colors } = useAppTheme();
   const navigation = useNavigation();
   const resetFilters = useTransactionsStore(state => state.resetFilters);
   const { filteredTransactions } = useGetTransactions();
+  const [type, setType] = useState<TTransactionType>('income');
+
+  // sort
+  filteredTransactions.sort(
+    (a, b) =>
+      new Date(a.transactionDate).getTime() -
+      new Date(b.transactionDate).getTime(),
+  );
 
   const handleBackPress = useCallback(
     (isEvent = false) => {
@@ -76,13 +88,10 @@ const Transactions = () => {
   }, [handleBackPress]);
 
   const pointer: Pointer = {
-    // autoAdjustPointerLabelPosition: true,
-    pointerLabelComponent: items => {
-      console.log({ items });
+    pointerLabelComponent: (items: Array<any>) => {
       return (
         <View
           style={{
-            zIndex: 100,
             height: 120,
             width: 100,
             backgroundColor: colors.surfaceVariant,
@@ -113,14 +122,7 @@ const Transactions = () => {
       <CommonHeader />
 
       {/* recent transactions section */}
-      <View
-        style={[
-          gs.fullFlex,
-          {
-            paddingHorizontal: spacing.lg,
-          },
-        ]}
-      >
+      <View style={[gs.fullFlex]}>
         {filteredTransactions.length < 2 ? (
           <View style={[gs.fullFlex, gs.centerItems]}>
             <Text
@@ -141,17 +143,115 @@ const Transactions = () => {
               marginTop: spacing.md,
             }}
           >
-            <LineChart
-              hideDataPoints
-              curved
-              curveType={CurveType.QUADRATIC}
-              adjustToWidth
-              hideRules
-              data2={expenseGraphData}
-              data={incomeGraphData}
-              pointerConfig={pointer}
-              noOfSections={4}
-            />
+            <View
+              style={[
+                {
+                  paddingHorizontal: spacing.md,
+                  marginBottom: spacing.md,
+                  gap: spacing.lg,
+                },
+                gs.flexRow,
+                gs.itemsCenter,
+              ]}
+            >
+              <PressableWithFeedback
+                style={[
+                  gs.fullFlex,
+                  styles.typeButton,
+                  {
+                    backgroundColor:
+                      type === 'income' ? colors.inversePrimary : 'transparent',
+                    borderWidth: 0.5,
+                    borderColor:
+                      type === 'income'
+                        ? 'transparent'
+                        : colors.onSurfaceDisabled,
+                  },
+                ]}
+                onPress={() => setType('income')}
+              >
+                <Text
+                  style={[
+                    gs.centerText,
+                    {
+                      color: colors.onPrimaryContainer,
+                    },
+                  ]}
+                >
+                  Income
+                </Text>
+              </PressableWithFeedback>
+              <PressableWithFeedback
+                style={[
+                  gs.fullFlex,
+                  styles.typeButton,
+                  {
+                    backgroundColor:
+                      type === 'expense'
+                        ? colors.inversePrimary
+                        : 'transparent',
+                    borderWidth: 0.5,
+                    borderColor:
+                      type === 'expense'
+                        ? 'transparent'
+                        : colors.onSurfaceDisabled,
+                  },
+                ]}
+                onPress={() => setType('expense')}
+              >
+                <Text
+                  style={[
+                    gs.centerText,
+                    {
+                      color: colors.onPrimaryContainer,
+                    },
+                  ]}
+                >
+                  Expense
+                </Text>
+              </PressableWithFeedback>
+            </View>
+            <View
+              style={{
+                paddingLeft: spacing.sm,
+              }}
+            >
+              <LineChart
+                renderDataPointsAfterAnimationEnds
+                curved
+                areaChart
+                dataPointsColor={
+                  type === 'income' ? colors.success : colors.error
+                }
+                isAnimated
+                animateOnDataChange
+                animationDuration={1000}
+                curveType={CurveType.QUADRATIC}
+                hideRules
+                data={type === 'expense' ? expenseGraphData : incomeGraphData}
+                pointerConfig={pointer}
+                adjustToWidth
+                noOfSections={4}
+                yAxisColor={colors.onSurfaceDisabled}
+                yAxisThickness={1}
+                yAxisTextStyle={{
+                  fontSize: textSize.xs,
+                  color: colors.onSurfaceDisabled,
+                }}
+                width={330}
+                xAxisColor={colors.onSurfaceDisabled}
+                startFillColor={
+                  type === 'expense' ? colors.error : colors.success
+                }
+                endFillColor={
+                  type === 'expense' ? colors.error : colors.success
+                }
+                startOpacity={0.5}
+                endOpacity={0.2}
+                color="transparent"
+                initialSpacing={0}
+              />
+            </View>
           </View>
         )}
       </View>
@@ -164,5 +264,9 @@ export default Transactions;
 const styles = StyleSheet.create({
   container: {
     paddingBottom: 200,
+  },
+  typeButton: {
+    paddingVertical: spacing.sm,
+    borderRadius: borderRadius.pill,
   },
 });
