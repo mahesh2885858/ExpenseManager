ffebbfa7b0135b789a49f1919187e47eaab43863
mahesh <mahesh2885858@gmail.com>
2026-01-18 17:29:04 +0530

    rename

--- ios/ExpenseManager/AppDelegate.swift
+++ ios/ExpenseManager/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "ExpenseManager",
+      withModuleName: "Expense Manager",
       in: window,
       launchOptions: launchOptions
     )
