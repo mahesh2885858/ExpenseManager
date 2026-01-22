a58277827684ca256cc0be9f5e8da596f87624fa
mahesh <mahesh2885858@gmail.com>
2026-01-22 21:23:00 +0530

    rename complete

--- android/app/src/main/java/io/github/mahesh2885858/expensemanager/MainActivity.kt
+++ android/app/src/main/java/io/github/mahesh2885858/expensemanager/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.expensemanager
+package io.github.mahesh2885858.expensemanager
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
