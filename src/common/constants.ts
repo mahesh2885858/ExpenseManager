86e35a9d77e59dcec96ffc03cc5b391f1ecba192
mahesh <mahesh2885858@gmail.com>
2026-01-31 12:23:00 +0530

    core: fix not validating data properly for importing/exporting data

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,3 +3,5 @@ export const DEFAULT_CATEGORY_ID = 'default_category_327065';
 export const CURRENCY_SYMBOL = '₹';
 export const MAX_DESCRIPTION_LIMIT = 150;
 export const MAX_AMOUNT_LENGTH = 9;
+export const APP_NAME_EXPORT_DATA = 'ExpenseManager';
+export const BACKUP_VERSION = 1;
